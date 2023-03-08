cc1fa3407b5b90698836ab30c57f2613edcf7b36
abhishek3898 <abhishek.t@permutehq.com>
2023-03-08 15:58:49 +0530

    database models create, order

--- sheets/models.py
+++ sheets/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from datetime import date, datetime, time
 
 # Create your models here.
+
+class Orders(models.Model):
+    firstname = models.CharField(max_length=100,null=False,blank=True)
+    lastname = models.CharField(max_length=100)
+    created_on = models.DateField(auto_now=True)
+    price = models.IntegerField(null=False)
+    status = models.CharField(max_length=100)
+    
+    def __str__(self):
+        self.firstname + " " + self.lastname
+    
+    class Meta:
+        db_table = "orders"
